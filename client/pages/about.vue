755e6ca185f559d36cc7e054a90b03529c477278
SotaSuzuki <3.3sota.suzuki@gmail.com>
2017-10-03 21:05:13 +0900

    add store: mode classic

--- client/pages/about.vue
+++ client/pages/about.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>About me</h1>
+  <div>
+    <h1>About me</h1>
+    <p>count: {{ $store.state.counter }}</p>
+    <button @click="$store.commit('increment')">push please</button>
+  </div>
 </template>
 
 <script>
